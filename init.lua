2b6e044927388e43a61f8e4d40f7a8a5f5e8c9e0
tianb2 <tianbo.dev@gmail.com>
2024-08-18 13:36:32 -0700

    format

--- init.lua
+++ init.lua
@@ -126,4 +126,4 @@ require 'nvim-treesitter.configs'.setup {
 
 -- floaterm
 vim.keymap.set('n', '<C-t>', ':FloatermToggle<CR>')
-vim.keymap.set('t', '<C-t>', '<C-\\><C-n>:FloatermToggle<CR>', {noremap = true, silent = true})
+vim.keymap.set('t', '<C-t>', '<C-\\><C-n>:FloatermToggle<CR>', { noremap = true, silent = true })
